302602fe37b83c2c70818d39226b346c7c09e439
fredyyfajarr <fredyfajar46@gmail.com>
2025-12-08 14:04:47 +0700

    update file size cannot up size

--- controllers/controller.py
+++ controllers/controller.py
@@ -1,5 +1,7 @@
 import streamlit as st
 from PIL import Image
+
+# Import Modul MVC
 from models.model import QuantizationModel
 from views.view import AppView
 
@@ -14,30 +16,32 @@ class AppController:
         self.view.render_header()
         
         if uploaded_file is not None:
-            with st.spinner('Memproses citra & visualisasi 3D...'):
+            # Spinner ini akan muncul sebentar saja karena ada Cache
+            with st.spinner('Memproses citra...'):
                 orig_img = Image.open(uploaded_file).convert('RGB')
                 
-                # 1. Proses Gambar
+                # --- TAMBAHAN KODE RESIZE (SOLUSI 1MB ERROR) ---
+                # Jika gambar terlalu besar, kita kecilkan agar ringan.
+                # Max lebar/tinggi 1000px sudah sangat cukup untuk tugas ini.
+                orig_img.thumbnail((1000, 1000)) 
+                # -----------------------------------------------
+                
+                # PANGGIL MODEL (Fungsi Static yang di-Cache)
                 result_data = self.model.process_image_cached(orig_img, bits)
                 
-                # 2. Hitung Metrik
+                # Hitung Metrik & Palette
                 mse, psnr = self.model.calculate_mse_psnr(
                     result_data['original_array'], 
                     result_data['reconstructed_array']
                 )
-                
-                # 3. Ekstrak Data untuk Visualisasi
                 palette = self.model.extract_palette(result_data['reconstructed_array'], 8)
                 
                 # --- DATA 3D BARU ---
-                # Mengambil sampel data untuk plot 3D (agar ringan, ambil 500 sampel saja)
                 df_3d_orig = self.model.get_3d_plot_data(result_data['original_array'], num_samples=500)
                 df_3d_res = self.model.get_3d_plot_data(result_data['reconstructed_array'], num_samples=500)
-                # --------------------
             
-            # 4. Render UI
+            # PANGGIL VIEW
             self.view.render_dashboard(bits, mse, psnr)
-            # Pass data 3D ke View
             self.view.render_tabs(orig_img, result_data, bits, palette, df_3d_orig, df_3d_res)
         
         else:
